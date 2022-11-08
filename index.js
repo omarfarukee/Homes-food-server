bfa7af00751e9276750ad8fabbf2b277218c6f6c
omarfarukee <umarfaruque1920@gmail.com>
2022-11-08 14:32:45 +0600

    data insert in mongo successfully

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -12,14 +13,23 @@ app.use(express.json());
 // console.log(process.env.DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xdpsuxi.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+        try{
+            const foodsCollection = client.db('foodFor').collection('foods')
 
+            app.get('/foods', async(req, res) =>{   
+                const query ={}
+                const cursor = foodsCollection.find(query);
+                const foods = await cursor.toArray();
+                res.send(foods)
+            })
+        }
+        finally{
+
+        }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) =>{
     res.send('homes foods in running ')
