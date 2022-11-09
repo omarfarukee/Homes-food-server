4d553f43b48c6bdb529d4a3f60d6a7705128c21f
omarfarukee <umarfaruque1920@gmail.com>
2022-11-09 06:55:23 +0600

    mongo api data load from backend

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run(){
         try{
             const foodsCollection = client.db('foodFor').collection('foods')
 
-            const ordersCollection =client.db('foodFor').collection('orders')
+            const reviewCollection =client.db('foodFor').collection('orders')
 
             app.get('/services', async(req, res) =>{   
                 const query ={}
@@ -35,10 +35,31 @@ async function run(){
                 const service = await foodsCollection.findOne(query);
                 res.send(service)
             })
+//--------------- review data load create -section---------------------------
 
-            app.post('/orders', async (req, res) =>{
+            // app.get('/review', async(req , res) =>{
+            //     const query = {}
+            //     const cursor = reviewCollection.find(query);
+            //     const reviews = await cursor.toArray();
+            //     res.send(reviews)
+            // })
+            
+            app.get('/review', async(req , res) =>{
+                console.log(req.query.email)
+                let query = {};
+                if(req.query.email){
+                    query ={
+                        email : req.query.email
+                    }
+                }
+                const cursor = reviewCollection.find(query);
+                const reviews = await cursor.toArray();
+                res.send(reviews)
+            })
+
+            app.post('/review', async (req, res) =>{
                 const order = req.body;
-                const result = await ordersCollection.insertOne(order);
+                const result = await reviewCollection.insertOne(order);
                 res.send(result)
             })
         }
